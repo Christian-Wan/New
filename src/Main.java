73fdec3f10c97afd022b3ced2f2cb915c7e7147b
Christian-Wan <email@email.com>
2023-09-19 13:04:13 -0400

    Added print statement on line 5 to state favorite food

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Christian Wan");
         System.out.println("Manchester by The Sea");
+        System.out.println("Rice");
     }
 }
